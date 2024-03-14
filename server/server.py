0934bc6bd9d1ad1463c33fb59edfdb5138cd0cf9
aashif000 <163309073+aashif000@users.noreply.github.com>
2024-03-14 15:21:29 +0530

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import util
+app = Flask(__name__)
+CORS(app)
+
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations' : util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict_home_price', methods = ['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server for Price Prediction")
+    util.load_saved_artifacts()
+    app.run()
